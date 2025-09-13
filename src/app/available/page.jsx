2123e84cdf47c026b44af8b6498aa671fa0fd5fa
Trang Vu <vulinhtrang2305@gmail.com>
2025-09-13 13:55:18 +0700

    feat : available box app

---
+++ src/app/available/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AvailableBox from '../../components/AvailableBox'
+
+function page() {
+  return (
+    <div>
+      <AvailableBox/>
+    </div>
+  )
+}
+
+export default page
