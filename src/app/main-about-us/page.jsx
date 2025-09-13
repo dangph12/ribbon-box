40209b0e130bba4aa625f31452b1d441769652e5
Trang Vu <vulinhtrang2305@gmail.com>
2025-09-13 13:43:33 +0700

    feat: layout about us

---
+++ src/app/main-about-us/page.jsx
@@ -0,0 +1,16 @@
+import AboutUs from "../../components/about-us";
+import AboutTeam from "../../components/AboutTeam";
+import HowToShop from "../../components/HowToShop";
+
+
+function MainAboutUs() {
+  return (
+    <div>
+      <AboutUs />
+      <HowToShop />
+      <AboutTeam />
+    </div>
+  );
+}
+
+export default MainAboutUs;
