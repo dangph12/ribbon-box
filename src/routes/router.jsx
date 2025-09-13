121ae29ff7d360cd2de5b3b4b4121e9603f4178e
Trang Vu <vulinhtrang2305@gmail.com>
2025-09-13 13:24:43 +0700

    feat: add homepage in router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,29 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter } from "react-router";
 
-import DesignGiftBox from '~/app/design-giftbox/page';
-import HomePage from '~/app/page';
+import DesignGiftBox from "~/app/design-giftbox/page";
+import HomePage from "~/app/page";
+import HomepageLayout from "../layouts/homepageLayout";
+import Homepage from "../app/homepage/page";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomePage />
+    path: "/",
+    element: <HomePage />,
   },
   {
-    path: '/design-giftbox',
-    element: <DesignGiftBox />
-  }
+    path: "/design-giftbox",
+    element: <DesignGiftBox />,
+  },
+  {
+    path: "/home",
+    element: <HomepageLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+    ],
+  },
 ]);
+
 export default router;
