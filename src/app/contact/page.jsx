b54d2555e43c5eb895095c3f88efd9fdb671673e
Trang Vu <vulinhtrang2305@gmail.com>
2025-09-13 13:48:33 +0700

    feat: contact us in app

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactUs from '../../components/ContactUs'
+
+function page() {
+  return (
+    <div>
+      <ContactUs/>
+    </div>
+  )
+}
+
+export default page
