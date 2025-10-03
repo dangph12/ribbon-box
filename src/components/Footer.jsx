da5f4cedefcf1481469f9dda22339de213c1c411
dangph12 <dangphhe181217@fpt.edu.vn>
2025-10-03 22:41:36 +0700

    fix: fix key in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,64 +1,67 @@
-import logo from "../assets/2.png";
-import { Link } from "react-router";
+import logo from '../assets/2.png';
+import { Link } from 'react-router';
 
 const footerItems = [
   {
-    title: "Theo dõi chúng tôi",
+    title: 'Theo dõi chúng tôi',
     links: [
-      { label: "Facebook", href: "https://www.facebook.com/profile.php?id=61580628163581" },
-      { label: "Tiktok", href: "https://www.tiktok.com/@ribbon.box5" },
+      {
+        label: 'Facebook',
+        href: 'https://www.facebook.com/profile.php?id=61580628163581'
+      },
+      { label: 'Tiktok', href: 'https://www.tiktok.com/@ribbon.box5' }
     ],
-    type: "external",
+    type: 'external'
   },
   {
-    title: "Điều hướng",
+    title: 'Điều hướng',
     links: [
-      { label: "Chính sách bảo mật", href: "#" },
-      { label: "Điều khoản & Điều kiện", href: "#" },
+      { label: 'Chính sách bảo mật', href: '#' },
+      { label: 'Điều khoản & Điều kiện', href: '#' }
     ],
-    type: "external",
-  },
+    type: 'external'
+  }
 ];
 
 function Footer() {
   return (
     <div>
-      <footer className="bg-[#FAE8D7]">
-        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-          <div className="md:flex md:justify-between">
-            <div className="mb-5 md:mb-0">
-              <a href="/" className="flex items-center">
-                <img src={logo} className="h-40 me-3" alt="RibbonBox Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#AD3542]">
+      <footer className='bg-[#FAE8D7]'>
+        <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
+          <div className='md:flex md:justify-between'>
+            <div className='mb-5 md:mb-0'>
+              <a href='/' className='flex items-center'>
+                <img src={logo} className='h-40 me-3' alt='RibbonBox Logo' />
+                <span className='self-center text-2xl font-semibold whitespace-nowrap text-[#AD3542]'>
                   Ribbon Box
                 </span>
               </a>
             </div>
 
-            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
-              {footerItems.map((section) => (
+            <div className='grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+              {footerItems.map(section => (
                 <div key={section.title}>
-                  <h2 className="mb-6 text-sm font-semibold text-[#AD3542] uppercase">
+                  <h2 className='mb-6 text-sm font-semibold text-[#AD3542] uppercase'>
                     {section.title}
                   </h2>
-                  <ul className="text-[#AD3542] font-medium space-y-2">
-                    {section.links.map((item) =>
-                      section.type === "internal" ? (
+                  <ul className='text-[#AD3542] font-medium space-y-2'>
+                    {section.links.map((item, index) =>
+                      section.type === 'internal' ? (
                         <li key={item?.to}>
                           <Link
                             to={item?.to}
-                            className="hover:text-[#C25C61] transition"
+                            className='hover:text-[#C25C61] transition'
                           >
                             {item.label}
                           </Link>
                         </li>
                       ) : (
-                        <li key={item.href}>
+                        <li key={`${item.label}-${index}`}>
                           <a
                             href={item.href}
-                            className="hover:text-[#C25C61] transition"
-                            target="_blank"
-                            rel="noopener noreferrer"
+                            className='hover:text-[#C25C61] transition'
+                            target='_blank'
+                            rel='noopener noreferrer'
                           >
                             {item.label}
                           </a>
@@ -71,12 +74,12 @@ function Footer() {
             </div>
           </div>
 
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <hr className='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
 
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-[#AD3542] sm:text-center">
-              © 2025{" "}
-              <a href="/" className="hover:text-[#C25C61]">
+          <div className='sm:flex sm:items-center sm:justify-between'>
+            <span className='text-sm text-[#AD3542] sm:text-center'>
+              © 2025{' '}
+              <a href='/' className='hover:text-[#C25C61]'>
                 Ribbon Box™
               </a>
               . Mọi quyền được bảo lưu.
